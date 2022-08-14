a6a7257d930ba358b5bcd2db0d016147f0c83278
Łukasz Piekarski <wookashp98@gmail.com>
2022-08-14 20:40:54 +0200

    Fix local drive

--- collector/collect.py
+++ collector/collect.py
@@ -22,15 +22,18 @@ def collect_group():
 @click.option("--name", "-n", help="Name of the created dataset. If none is provided defaults to the yyyy-MM-dd-HH date")
 def collect(date: str, name: str):
     if date == "latest":
-        end_date = datetime.datetime.now()
-        start_date = datetime.datetime.now() - datetime.timedelta(hours=1)
+        LOGGER.info(f"Collecting latest data")
+        df = yf.download(tickers='^GSPC', period='1h', interval='1h', prepost=True).tail(1)
+        start_date = df.index[0]
     else:
         start_date = parser.parse(date)
         end_date = start_date + datetime.timedelta(hours=1)
-    start_date = start_date.replace(minute=0, second=0, microsecond=0)
-    end_date = end_date.replace(minute=0, second=0, microsecond=0)
-    LOGGER.info(f"Collecting data for period {start_date} - {end_date}")
-    df = yf.download(tickers='^GSPC', start=start_date, end=end_date, interval='1h')
+        start_date = start_date.replace(minute=0, second=0, microsecond=0)
+        end_date = end_date.replace(minute=0, second=0, microsecond=0)
+        LOGGER.info(f"Collecting data for period {start_date} - {end_date}")
+        df = yf.download(tickers='^GSPC', period='1h', interval='1h', prepost=True).tail(1)
+    if df.empty:
+        raise BotErrorWithoutStacktrace("Failed to collect data")
     if name is None:
         name = start_date.strftime("%Y-%m-%d-%H")
     put_dataset(name, df)
--- commons/drive/local.py
+++ commons/drive/local.py
@@ -7,10 +7,13 @@ __all__ = ["upload", "download"]
 LOCAL_STORE = './data'
 
 def upload(local_path: str, cloud_path: str):
-	shutil.copy(local_path, cloud_path)
+	target = os.path.join(LOCAL_STORE, cloud_path)
+	if os.path.normpath(local_path) != os.path.normpath(target):
+		shutil.copy(local_path, target)
 
 def download(cloud_path: str, local_path: str):
 	source = os.path.join(LOCAL_STORE, cloud_path)
 	if not os.path.exists(source):
 		raise CloudFileNotFoundError(f"Cannot find file in remote location: '{source}'")
-	shutil.copy(local_path, source)
+	if os.path.normpath(local_path) != os.path.normpath(source):
+		shutil.copy(source, local_path)
