12febe86452ab24320756d68f2658b1b34fd731e
Łukasz Piekarski <wookashp98@gmail.com>
2022-08-14 21:37:53 +0200

    Change data storage location

--- commons/drive/local.py
+++ commons/drive/local.py
@@ -1,17 +1,20 @@
 import shutil
 import os
+
+from commons.env import getenv
 from commons.exceptions import CloudFileNotFoundError
 
 __all__ = ["upload", "download"]
 
-LOCAL_STORE = './data'
 
 def upload(local_path: str, cloud_path: str):
+	LOCAL_STORE = getenv("DATA_STORAGE_DIR", './data')
 	target = os.path.join(LOCAL_STORE, cloud_path)
 	if os.path.normpath(local_path) != os.path.normpath(target):
 		shutil.copy(local_path, target)
 
 def download(cloud_path: str, local_path: str):
+	LOCAL_STORE = getenv("DATA_STORAGE_DIR", './data')
 	source = os.path.join(LOCAL_STORE, cloud_path)
 	if not os.path.exists(source):
 		raise CloudFileNotFoundError(f"Cannot find file in remote location: '{source}'")
