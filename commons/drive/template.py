ec021a2e507ab8c107de281d14c1b1165e8aa22a
Łukasz Piekarski <wookashp98@gmail.com>
2022-08-15 22:55:14 +0200

    Run tests on push and introduce template modules

---
+++ commons/drive/template.py
@@ -0,0 +1,8 @@
+__all__ = ["upload", "download"]
+
+
+def upload(local_path: str, cloud_path: str) -> None:
+    pass
+
+def download(cloud_path: str, local_path: str) -> None:
+    pass
--- model/predictors/template.py
+++ model/predictors/template.py
@@ -1,10 +1,8 @@
 import pandas as pd
-import numpy as np
+
 
 def predict(X: pd.DataFrame) -> pd.DataFrame:
-    df = pd.DataFrame()
-    df["y"] = np.zeros_like(X["Close"])
-    return df
+    return pd.DataFrame()
 
 
 def train(X: pd.DataFrame, y: pd.DataFrame) -> None:
