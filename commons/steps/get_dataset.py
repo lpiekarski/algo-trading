5101b14e79846ce46910ccf7ef172dbb3623cd16
Łukasz Piekarski <wookashp98@gmail.com>
2022-08-29 22:02:57 +0200

    Fix get_dataset recursion bug

--- commons/steps/get_dataset.py
+++ commons/steps/get_dataset.py
@@ -1,6 +1,6 @@
 import logging
 
-from commons.dataset import get_dataset
+import commons.dataset as ds
 from commons.timing import step
 
 LOGGER = logging.getLogger(__name__)
@@ -8,4 +8,4 @@ LOGGER = logging.getLogger(__name__)
 @step
 def get_dataset(dataset=None, *args, **kwargs):
     LOGGER.info("Getting dataset")
-    return dict(X=get_dataset(dataset))
+    return dict(X=ds.get_dataset(dataset))
