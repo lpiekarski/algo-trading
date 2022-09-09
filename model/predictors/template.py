bc7af0ceb50fa266a5d12afe33d9c8c6012d1be2
Łukasz Piekarski <wookashp98@gmail.com>
2022-09-09 08:17:46 +0200

    wip

--- model/predictors/template.py
+++ model/predictors/template.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 
 def predict(X: pd.DataFrame) -> pd.DataFrame:
-    return pd.DataFrame()
+    res = pd.DataFrame()
+    res['y_pred'] = X["Open"]
+    return res
 
 
 def train(X: pd.DataFrame, y: pd.DataFrame) -> None:
