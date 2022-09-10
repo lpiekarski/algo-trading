1dbfff97e7637bafbdc675d6bd24ce3618bc2f44
Łukasz Piekarski <wookashp98@gmail.com>
2022-09-10 10:13:47 +0200

    Implement drive based on git-lfs
    1. Implement git-lfs drive with upload and download operations
    2. Modify workflows so that environment secrets are visible
    3. Implement git api in /commons/git.py
    
    https://github.com/lpiekarski/SP2137/issues/10

---
+++ commons/drive/git.py
@@ -0,0 +1,37 @@
+import logging
+import shutil
+import os
+import commons.git as git
+from commons.env import getenv
+
+__all__ = ["upload", "download"]
+
+
+LOGGER = logging.getLogger(__name__)
+REPO_PATH = "./.git-drive"
+
+def initialize():
+    if not os.path.exists(REPO_PATH):
+        LOGGER.info(os.environ)
+        REPO_URL = f"https://{getenv('GIT_PASSWORD')}@github.com/S-P-2137/Data"
+        git.clone(REPO_URL, REPO_PATH)
+
+def upload(local_path: str, cloud_path: str) -> None:
+    initialize()
+    add_path = os.path.join(REPO_PATH, cloud_path)
+    if os.path.normpath(local_path) != os.path.normpath(add_path):
+        os.makedirs(os.path.dirname(add_path), exist_ok=True)
+        shutil.copy(local_path, add_path)
+    git.add(os.path.abspath(add_path), cwd=REPO_PATH)
+    git.commit(f"Automated: add '{cloud_path}' to the storage", cwd=REPO_PATH)
+    git.push(cwd=REPO_PATH)
+
+def download(cloud_path: str, local_path: str) -> None:
+    initialize()
+    checked_out_path = os.path.join(REPO_PATH, cloud_path)
+    if not os.path.exists(checked_out_path):
+        git.fetch(cwd=REPO_PATH)
+        git.checkout(cloud_path, cwd=REPO_PATH)
+    if os.path.normpath(local_path) != os.path.normpath(checked_out_path):
+        os.makedirs(os.path.dirname(local_path), exist_ok=True)
+        shutil.copy(checked_out_path, local_path)
---
+++ commons/git.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def clone(url: str, path: str, base: str="git", **kwargs):
+    subprocess.run([base, "clone", url, path], **kwargs).check_returncode()
+
+def fetch(base: str="git", **kwargs):
+    subprocess.run([base, "fetch"], **kwargs).check_returncode()
+
+def checkout(path: str, base: str="git", **kwargs):
+    subprocess.run([base, "checkout", "origin/main", "--", path], **kwargs).check_returncode()
+
+def add(path: str, base: str="git", **kwargs):
+    subprocess.run([base, "add", path], **kwargs).check_returncode()
+
+def commit(message: str, base: str="git", **kwargs):
+    subprocess.run([base, "commit", "-m", message], **kwargs).check_returncode()
+
+def push(base: str="git", **kwargs):
+    subprocess.run([base, "push", "origin", "main"], **kwargs).check_returncode()
