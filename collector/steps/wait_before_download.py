47205f568245ecd74871c6f4c9afde02b65efc0b
Łukasz Piekarski <wookashp98@gmail.com>
2022-09-12 21:34:42 +0200

    Recalculate time left for sleep

--- collector/steps/wait_before_download.py
+++ collector/steps/wait_before_download.py
@@ -11,14 +11,13 @@ def wait_before_download(skip_wait=None, *args, **kwargs):
         LOGGER.info("Skipping.")
         return
     LOGGER.info("Waiting for the correct time to download the data")
-    time = datetime.datetime.now()
-    sleep_time_in_sec = 3600 - (time.minute * 60 + time.second)
-    LOGGER.info(f"Sleeping for a total of {sleep_time_in_sec} seconds.")
-    while sleep_time_in_sec > 0:
+    while True:
+        time = datetime.datetime.now()
+        sleep_time_in_sec = 3600 - (time.minute * 60 + time.second)
+        LOGGER.info(f"\t\t{sleep_time_in_sec} seconds left.")
         if sleep_time_in_sec < 60:
             sleep(sleep_time_in_sec)
+            LOGGER.info(f"Sleep is complete.")
+            break
         else:
             sleep(60)
-            sleep_time_in_sec -= 60
-            LOGGER.info(f"\t\t{sleep_time_in_sec} seconds left.")
-    LOGGER.info(f"Sleep is complete.")
