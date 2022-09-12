36c8fb552c65d2fa1794a48ede4c37fef61881b5
Łukasz Piekarski <wookashp98@gmail.com>
2022-09-12 14:10:44 +0200

    wip

--- model/predictors/zero.py
+++ model/predictors/zero.py
@@ -1,11 +1,10 @@
 import pandas as pd
-
+import numpy as np
 
 def predict(X: pd.DataFrame) -> pd.DataFrame:
     res = pd.DataFrame()
-    res['y_pred'] = X["Open"]
+    res['y_pred'] = np.zeros_like(X["Close"])
     return res
-
 
 def train(X: pd.DataFrame, y: pd.DataFrame) -> None:
     pass
