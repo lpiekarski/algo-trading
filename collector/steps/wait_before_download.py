c11d1af21806c1c8c07a475d35a2dab4990c1eca
Łukasz Piekarski <wookashp98@gmail.com>
2022-09-12 23:01:18 +0200

    Fix bug in hour countdown

--- collector/steps/wait_before_download.py
+++ collector/steps/wait_before_download.py
@@ -15,7 +15,7 @@ def wait_before_download(skip_wait=None, *args, **kwargs):
         time = datetime.datetime.now()
         sleep_time_in_sec = 3600 - (time.minute * 60 + time.second)
         LOGGER.info(f"\t\t{sleep_time_in_sec} seconds left.")
-        if sleep_time_in_sec < 60:
+        if sleep_time_in_sec <= 60:
             sleep(sleep_time_in_sec)
             LOGGER.info(f"Sleep is complete.")
             break
