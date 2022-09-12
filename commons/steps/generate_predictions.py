444256653211a7b36781e05c89b90c9b9953e2b7
Łukasz Piekarski <wookashp98@gmail.com>
2022-09-12 20:31:17 +0200

    wip

--- commons/steps/generate_predictions.py
+++ commons/steps/generate_predictions.py
@@ -1,13 +1,11 @@
 import logging
 
 from commons.timing import step
-from model.predictors import get_model_module
 
 LOGGER = logging.getLogger(__name__)
 
 @step
-def generate_predictions(model=None, X=None, *args, **kwargs):
-    LOGGER.info("Generating predictions from model")
-    model_module = get_model_module(model)
+def generate_predictions(model=None, model_module=None, X=None, *args, **kwargs):
+    LOGGER.info(f"Generating predictions from model '{model}'")
     y_pred = model_module.predict(X)
     return dict(y_pred=y_pred)
--- model/predict.py
+++ model/predict.py
@@ -4,6 +4,8 @@ from commons.steps.generate_predictions import generate_predictions
 from commons.steps.get_dataset import get_dataset
 from commons.steps.process_parameter import process_parameter
 from commons.timing import subcommand
+from model.steps.download_model import download_model
+from model.steps.get_model_module import get_model_module
 from model.steps.save_prediction_result import save_prediction_result
 
 __all__ = ["predict_group"]
@@ -19,7 +21,9 @@ def predict_group(): pass
     process_parameter('model'),
     process_parameter('dataset'),
     process_parameter('output', optional=True),
+    get_model_module,
     get_dataset,
+    download_model,
     generate_predictions,
     save_prediction_result
 ])
