231b080650e4ab7a6f70ac1db05b677a3cc29fa0
Łukasz Piekarski <wookashp98@gmail.com>
2022-10-11 13:01:58 +0200

    Remove code repetition in technical_indicators

--- collector/technical_indicators/technical_indicators.py
+++ collector/technical_indicators/technical_indicators.py
@@ -1,9 +1,9 @@
-# libraries
+import sys
+
 import pandas as pd
 import pandas_ta as pta
 import numpy as np
 
-# Keltner Channel
 def KELCH(df, n):
     KelChM = pd.Series(((df['High'] + df['Low'] + df['Close']) / 3), name='KelChM_' + str(n)).rolling(window=n).mean()
     KelChU = pd.Series(((4 * df['High'] - 2 * df['Low'] + df['Close']) / 3),
@@ -17,19 +17,15 @@ def KELCH(df, n):
 
 def add_sma(df, time_tag, length):
     df[f'SMA_{length}_{time_tag}'] = pta.sma(df["Close"], length=length)
-    return df
 
 def add_ema(df, time_tag, length):
     df[f'EMA_{length}_{time_tag}'] = pta.ema(df["Close"], length=length)
-    return df
 
 def add_dema(df, time_tag, length):
     df[f'DEMA_{length}_{time_tag}'] = pta.dema(df["Close"], length=length)
-    return df
 
 def add_kama(df, time_tag, length):
     df[f'kama_{length}_{time_tag}'] = pta.kama(df["Close"], length=length)
-    return df
 
 def add_bolinger_bands(df, time_tag):
     bbands_result = pta.bbands(df["Close"], timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
@@ -38,7 +34,6 @@ def add_bolinger_bands(df, time_tag):
     df[f'Lowerband_{time_tag}'] = bbands_result['BBU_5_2.0']
     df[f'Bandwidth_{time_tag}'] = bbands_result['BBB_5_2.0']
     df[f'Percent_Column_BBands_{time_tag}'] = bbands_result['BBP_5_2.0']
-    return df
 
 def add_ichimoku(df, time_tag):
     ichimoku_result = pta.ichimoku(df['High'], df['Low'], df['Close'])
@@ -46,7 +41,6 @@ def add_ichimoku(df, time_tag):
     df[f'Ichimoku_ISB_26_{time_tag}'] = ichimoku_result[0]['ISB_26']
     df[f'Ichimoku_ITS_9_{time_tag}'] = ichimoku_result[0]['ITS_9']
     df[f'Ichimoku_IKS_26_{time_tag}'] = ichimoku_result[0]['IKS_26']
-    return df
 
 def add_parabolic_sar(df, time_tag):
     PSAR = pta.psar(df['High'], df['Low'])
@@ -54,63 +48,49 @@ def add_parabolic_sar(df, time_tag):
     df[f'PSARs_0.02_0.2_{time_tag}'] = PSAR['PSARs_0.02_0.2']
     df[f'PSARaf_0.02_0.2_{time_tag}'] = PSAR['PSARaf_0.02_0.2']
     df[f'PSARr_0.02_0.2_{time_tag}'] = PSAR['PSARr_0.02_0.2']
-    return df
 
 def add_stdev(df, time_tag, length):
     df[f'Standard_deviation_{length}_{time_tag}'] = pta.stdev(df['Close'], length=length)
-    return df
 
 def add_linreg(df, time_tag, length):
     df[f'Linear_Regression_{length}_{time_tag}'] = pta.linreg(df['Close'], length=length)
-    return df
 
 def add_atr(df, time_tag, length):
     df[f'Average_True_Range_{length}_{time_tag}'] = pta.atr(df['High'], df['Low'], df['Close'], length=length)
-    return df
 
 def add_rsi(df, time_tag, length):
     df[f'rsi_{length}_{time_tag}'] = pta.rsi(df['Close'], length=length)
-    return df
 
 def add_cci(df, time_tag, length):
     df[f'Commodity_Channel_Index_{length}_{time_tag}'] = pta.cci(df['High'], df['Low'], df['Close'], length=length)
-    return df
-
 
 def add_momentum(df, time_tag, length):
     df[f'Momentum_{length}_{time_tag}'] = pta.mom(df['Close'], length=length)
-    return df
 
 def add_macd(df, time_tag):
     macd_result = pta.macd(df['Close'])
     df[f'MACD_12_26_9_{time_tag}'] = macd_result['MACD_12_26_9']
     df[f'MACDh_12_26_9_{time_tag}'] = macd_result['MACDh_12_26_9']
     df[f'MACDs_12_26_9_{time_tag}'] = macd_result['MACDs_12_26_9']
-    return df
 
 def add_stochrsi(df, time_tag, length=14, rsi_length=14):
     Stoch_RSI = pta.stochrsi(df['Close'], length=length, rsi_length=rsi_length)
     df[f'stoch_rsi_K%_{length}_{time_tag}'] = Stoch_RSI[f'STOCHRSIk_{length}_{rsi_length}_3_3']
     df[f'stoch_rsi_D%_{length}_{time_tag}'] = Stoch_RSI[f'STOCHRSId_{length}_{rsi_length}_3_3']
-    return df
 
 def add_stoch(df, time_tag):
     Stoch = pta.stoch(df['High'], df['Low'], df['Close'])
     df[f'STOCHk_14_3_3_{time_tag}'] = Stoch['STOCHk_14_3_3']
     df[f'STOCHd_14_3_3_{time_tag}'] = Stoch['STOCHd_14_3_3']
-    return df
 
 def add_rvi(df, time_tag, length):
     df[f'RVI_{length}_{time_tag}'] = pta.rvi(df['Close'], length=length)
-    return df
 
 def add_willr(df, time_tag, length):
     df[f'William_R_{length}_{time_tag}'] = pta.willr(df['High'], df['Low'], df['Close'], length=length)
-    return df
 
 def add_ao(df, time_tag):
     df[f'Awesome_Oscillator_{time_tag}'] = pta.ao(df['High'], df['Low'], )
-    return df
 
 def add_ha(df, time_tag):
     heikin_result = pta.ha(df['Open'], df['High'], df['Low'], df['Close'])
@@ -118,36 +98,30 @@ def add_ha(df, time_tag):
     df[f'HA_high_{time_tag}'] = heikin_result['HA_high']
     df[f'HA_low_{time_tag}'] = heikin_result['HA_low']
     df[f'HA_close_{time_tag}'] = heikin_result['HA_close']
-    return df
 
 def add_donchian(df, time_tag):
     donchian_result = pta.donchian(df['High'], df['Low'])
     df[f'DCL_20_20_{time_tag}'] = donchian_result['DCL_20_20']
     df[f'DCM_20_20_{time_tag}'] = donchian_result['DCM_20_20']
     df[f'DCU_20_20_{time_tag}'] = donchian_result['DCU_20_20']
-    return df
 
 def add_KELCH(df, time_tag):
     kelch = KELCH(df, 20)
     df[f'KelChM_20_{time_tag}'] = kelch['KelChM_20']
     df[f'KelChU_20_{time_tag}'] = kelch['KelChU_20']
     df[f'KelChD_20_{time_tag}'] = kelch['KelChD_20']
-    return df
 
 def add_bop(df, time_tag):
     df[f'Balance_of_power_{time_tag}'] = pta.bop(df['Open'], df['High'], df['Low'], df['Close'])
-    return df
 
 def add_uo(df, time_tag):
     df[f'Ultimate_Oscillator_{time_tag}'] = pta.uo(df['High'], df['Low'], df['Close'])
-    return df
 
 def add_accbands(df, time_tag):
     acceleration_bands = pta.accbands(df['High'], df['Low'], df['Close'])
     df[f'ACCBL_20_{time_tag}'] = acceleration_bands['ACCBL_20']
     df[f'ACCBM_20_{time_tag}'] = acceleration_bands['ACCBM_20']
     df[f'ACCBU_20_{time_tag}'] = acceleration_bands['ACCBU_20']
-    return df
 
 def add_date_based(df, time_tag):
     timefunc = lambda name, sincos, length: sincos(getattr(df['Date'].dt, name) / length * 2 * np.pi)
@@ -164,113 +138,46 @@ def add_date_based(df, time_tag):
 
 # add technical indicators for all
 def add_technical_indicators(df, time_tag):
-    ### Trend indicators:
-    # Moving average
-    df = add_sma(df, time_tag, 10)
-    df = add_sma(df, time_tag, 20)
-    df = add_sma(df, time_tag, 50)
-    df = add_sma(df, time_tag, 100)
-    df = add_sma(df, time_tag, 200)
+    indicators = dict(
+        add_sma=[10, 20, 50, 100, 200],
+        add_ema=[10, 20, 50, 100, 200],
+        add_dema=[10, 20, 50, 100, 200],
+        add_kama=[10, 20, 50, 100, 200],
+        add_bolinger_bands=None,
+        add_ichimoku=None,
+        add_parabolic_sar=None,
+        add_stdev=[10, 20, 50, 100, 200],
+        add_linreg=[10, 20, 50, 100, 200],
+        add_atr=[14],
+        add_rsi=[14, 26],
+        add_cci=[20, 50],
+        add_momentum=[10, 14, 21],
+        add_macd=None,
+        add_stochrsi=[14, [46, 46]],
+        add_stoch=None,
+        add_rvi=[14],
+        add_willr=[14],
+        add_ao=None,
+        add_ha=None,
+        add_donchian=None,
+        add_KELCH=None,
+        add_bop=None,
+        add_uo=None,
+        add_accbands=None,
+        add_date_based=None
+    )
 
-    # Exponential moving average
-    df = add_ema(df, time_tag, 10)
-    df = add_ema(df, time_tag, 20)
-    df = add_ema(df, time_tag, 50)
-    df = add_ema(df, time_tag, 100)
-    df = add_ema(df, time_tag, 200)
-
-    # Double Exponential moving average
-    df = add_dema(df, time_tag, 10)
-    df = add_dema(df, time_tag, 20)
-    df = add_dema(df, time_tag, 50)
-    df = add_dema(df, time_tag, 100)
-    df = add_dema(df, time_tag, 200)
-
-    # Kaufman's Adaptive Moving Average
-    df = add_kama(df, time_tag, 10)
-    df = add_kama(df, time_tag, 20)
-    df = add_kama(df, time_tag, 50)
-    df = add_kama(df, time_tag, 100)
-    df = add_kama(df, time_tag, 200)
-
-    # Bolinger Bands
-    df = add_bolinger_bands(df, time_tag)
-
-    # Ichimoku
-    df = add_ichimoku(df, time_tag)
-
-    # Parabolic SAR
-    df = add_parabolic_sar(df, time_tag)
-
-    # Standard deviation
-    df = add_stdev(df, time_tag, 10)
-    df = add_stdev(df, time_tag, 20)
-    df = add_stdev(df, time_tag, 50)
-    df = add_stdev(df, time_tag, 100)
-    df = add_stdev(df, time_tag, 200)
-
-    # Regression
-    df = add_linreg(df, time_tag, 10)
-    df = add_linreg(df, time_tag, 20)
-    df = add_linreg(df, time_tag, 50)
-    df = add_linreg(df, time_tag, 100)
-    df = add_linreg(df, time_tag, 200)
-
-    # more?
-    ###Oscillators:
-    # Average True Range
-    df = add_atr(df, time_tag, 14)
-
-    # RSI 14
-    df = add_rsi(df, time_tag, 14)
-    df = add_rsi(df, time_tag, 26)
-
-    # Commodity Channel Index
-    df = add_cci(df, time_tag, 20)
-    df = add_cci(df, time_tag, 50)
-
-    # Momentum
-    df = add_momentum(df, time_tag, 10)
-    df = add_momentum(df, time_tag, 14)
-    df = add_momentum(df, time_tag, 21)
-
-    # MACD
-    df = add_macd(df, time_tag)
-
-    # Stochastic RSI
-    df = add_stochrsi(df, time_tag, 14)
-    df = add_stochrsi(df, time_tag, length=46, rsi_length=46)
-
-    # Stochastic
-    df = add_stoch(df, time_tag)
-
-    # Relative Vigor Index
-    df = add_rvi(df, time_tag, 14)
-
-    # R Williams
-    df = add_willr(df, time_tag, 14)
-
-    ###Volumes ?
-    ### others: (mainly mixes of both)
-    # Awesome Oscillator
-    df = add_ao(df, time_tag)
-
-    # Heikin Ashi
-    df = add_ha(df, time_tag)
-
-    # Donchian Channel
-    df = add_donchian(df, time_tag)
-
-    # Keltner Channel
-    df = add_KELCH(df, time_tag)
-
-    # Balance of power
-    df = add_bop(df, time_tag)
-
-    # Ultimate oscillator
-    df = add_uo(df, time_tag)
-
-    # Acceleration Bands
-    df = add_accbands(df, time_tag)
+    for indicator, params in indicators:
+        indicator_func = getattr(sys.modules[__name__], indicator)
+        if params is None:
+            indicator_func(df, time_tag)
+        else:
+            for param in params:
+                if isinstance(param, list):
+                    indicator_func(df, time_tag, *param)
+                elif isinstance(param, dict):
+                    indicator_func(df, time_tag, **param)
+                else:
+                    indicator_func(df, time_tag, param)
 
     return df
