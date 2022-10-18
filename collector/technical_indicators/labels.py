7bb0b9a82f6af0947d88dd530b35502d919f7610
Łukasz Piekarski <wookashp98@gmail.com>
2022-10-18 14:47:49 +0200

    wip

--- collector/technical_indicators/labels.py
+++ collector/technical_indicators/labels.py
@@ -5,8 +5,7 @@ import pandas as pd
 import bisect
 from commons.dataset import Dataset
 import logging
-
-from commons.progress_bar import ProgressBar
+from tqdm import tqdm
 
 LOGGER = logging.getLogger(__name__)
 
@@ -22,13 +21,11 @@ def find_indexes_with_price_percentage_change(df: pd.DataFrame, pct_change, dire
     hanging = []
     furthest_col = 'High' if direction == 1 else 'Low'
     LOGGER.info(f"Finding indexes with price percentage change for pct_change={pct_change} and direction={direction}")
-    progress_bar = ProgressBar(df.shape[0])
-    for index, row in df.iterrows():
+    for index, row in tqdm(df.iterrows(), total=df.shape[0]):
         while hanging and direction * row[furthest_col] >= direction * hanging[0]['price'] * (1 + direction * pct_change):
             top = hanging.pop(0)
             result[top['index']] = index
         insert(hanging, {'index': index, 'price': row['Close']}, direction)
-        progress_bar.update()
     result_df = pd.DataFrame(index=df.index.copy())
     result_df['result'] = None
     for k, v in result.items():
