e30f829ef0927a294648bc33a7038dd72d2956e3
Łukasz Piekarski <wookashp98@gmail.com>
2022-10-30 17:18:22 +0100

    Fix predict bugs

--- model/steps/save_prediction_result.py
+++ model/steps/save_prediction_result.py
@@ -2,6 +2,7 @@ import datetime
 import logging
 from commons.drive_utils.predictions import upload_prediction
 from commons.timing import step
+import pandas as pd
 
 LOGGER = logging.getLogger(__name__)
 
@@ -9,6 +10,6 @@ LOGGER = logging.getLogger(__name__)
 def save_prediction_result(output, model, dataset, y_pred, **kwargs):
     if output is None:
         time = datetime.datetime.now()
-        output = f"{dataset}_results_{model}_{time.year}_{time.month}_{time.day}_{time.hour}_{time.minute}"
+        output = f"prediction_{model}_{time.year}_{time.month}_{time.day}_{time.hour}_{time.minute}"
     LOGGER.info(f"Saving results as '{output}'")
-    upload_prediction(output, y_pred)
+    upload_prediction(output, pd.DataFrame(y_pred))
