6c22c299417d0fce8b8baeb9900acae9aa1f9da7
Łukasz Piekarski <wookashp98@gmail.com>
2022-11-09 22:17:24 +0100

    Add technical indicators tests

---
+++ tests/collector/technical_indicators/test_technical_indicators.py
@@ -0,0 +1,17 @@
+import collector.technical_indicators.technical_indicators as ti
+import testing.mocks as mocks
+
+def test_technical_indicators_added():
+    dataset = mocks.dataset()
+    time_tag = 'test'
+    pre_add_columns = dataset.df.shape[1]
+    ti.add_technical_indicators(dataset, time_tag)
+    post_add_columns = dataset.df.shape[1]
+    assert post_add_columns > pre_add_columns
+
+def test_technical_indicators_have_values():
+    dataset = mocks.dataset()
+    time_tag = 'test'
+    ti.add_technical_indicators(dataset, time_tag)
+    df = dataset.df.dropna(axis=1, how='all')
+    assert dataset.df.shape[1] == df.shape[1]
