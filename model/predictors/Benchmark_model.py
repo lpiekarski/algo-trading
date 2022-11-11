ba252e1afb5cab7095dd779123eba124f36b1e7c
jmaskiew <109626411+jmaskiew@users.noreply.github.com>
2022-11-11 15:29:19 +0100

    place for benchmark model. As I need to reinstall sp2137 let it be here

---
+++ model/predictors/Benchmark_model.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import lightgbm as lgbm
+import os
+
+from commons.data.preprocessor import Preprocessor
+
+# benchmark model
+
+def predict(x: pd.DataFrame) -> np.ndarray:
+    return
+
+def train(x: pd.DataFrame, y: pd.DataFrame) -> None:
+    return
+
+def save(path: str) -> None:
+    return
+
+def load(path: str) -> None:
+    return
