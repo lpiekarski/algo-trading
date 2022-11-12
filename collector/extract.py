a73ad19f3de3cf610775fe90c7d9964b07d7878a
jmaskiew <109626411+jmaskiew@users.noreply.github.com>
2022-11-12 19:17:05 +0100

    new type of trading method for benchmark

--- collector/extract.py
+++ collector/extract.py
@@ -20,10 +20,12 @@ def extract_group(): pass
 @option("--time-tag", "-t", default='1h', help="Rescale data to hours, days, month")
 @option("--dataset", "-d", help="Dataset to extract features from")
 @option("--append", "-a", default=True, help="Whether to overwrite or append to an existing dataset with the same name", is_flag=True)
+@option("--indicators", "-i", help="Path to the indicators configuration json file")
 @subcommand([
     process_parameter("time_tag"),
     process_parameter("name", optional=True),
     process_parameter("dataset"),
+    process_parameter("indicators", optional=True),
     get_dataset,
     add_indicators,
     add_labels,
--- model/predictors/benchmark.py
+++ model/predictors/benchmark.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
-import lightgbm as lgbm
-import os
-
-from commons.data.preprocessor import Preprocessor
 
 # benchmark model
+
+
+
 
 def predict(x: pd.DataFrame) -> np.ndarray:
     return
@@ -15,7 +14,6 @@ def train(x: pd.DataFrame, y: pd.DataFrame) -> None:
 
 def save(path: str) -> None:
     return
-
 
 def load(path: str) -> None:
     return
