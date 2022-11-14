a286e8b10b2236ebee0cc941b1e8116bc5f7bd3e
Łukasz Piekarski <wookashp98@gmail.com>
2022-11-14 14:17:54 +0100

    wip

--- commons/string.py
+++ commons/string.py
@@ -43,9 +43,3 @@ def padded(text: str, pad_symbol: str, length: int) -> str:
 
 def formpath(path):
     return os.path.abspath(os.path.normpath(path))
-
-def to_camel_case(snake_str):
-    components = snake_str.split('_')
-    # We capitalize the first letter of each component except the first one
-    # with the 'title' method and join them together.
-    return components[0] + ''.join(x.title() for x in components[1:])
