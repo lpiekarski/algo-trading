916729066fff88362d0c4171e963500e9720ef92
Łukasz Piekarski <wookashp98@gmail.com>
2022-12-14 22:06:58 +0100

    Add ability to customize indicators

--- collector/steps/add_labels.py
+++ collector/steps/add_labels.py
@@ -1,11 +1,31 @@
 import logging
-
-import collector.technical_indicators.labels as ls
+import yaml
 from commons.timing import step
+import sys
 
 LOGGER = logging.getLogger(__name__)
+DEFAULT_LABEL_CONFIG = dict(
+    best_decision=[0.01],
+)
 
 
 @step
-def add_labels(dataset, **kwargs):
-    ls.add_labels(dataset)
+def add_labels(dataset, labels, **kwargs):
+    label_config = None
+    if labels is not None:
+        with open(labels, 'r') as f:
+            label_config = yaml.load(f, yaml.CLoader)
+    if label_config is None:
+        label_config = DEFAULT_LABEL_CONFIG
+    for label, params in label_config.items():
+        label_func = getattr(sys.modules[__name__], label)
+        if params is None:
+            label_func(dataset)
+        else:
+            for param in params:
+                if isinstance(param, list):
+                    label_func(dataset, *param)
+                elif isinstance(param, dict):
+                    label_func(dataset, **param)
+                else:
+                    label_func(dataset, param)
