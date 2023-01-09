691ee53da9f0c3305726b05d8c9d995dcf8d429b
Łukasz Piekarski <wookashp98@gmail.com>
2023-01-09 19:34:21 +0100

    Swap os.environ.get with os.getenv

--- testing/steps/integration_tests.py
+++ testing/steps/integration_tests.py
@@ -9,7 +9,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 def integration_tests(*args, **kwargs):
-    log_dir = os.path.dirname(os.path.abspath(os.environ.get("LOG_FILE", "./logs/atf.log")))
+    log_dir = os.path.dirname(os.path.abspath(os.getenv("LOG_FILE", "./logs/atf.log")))
     with zipfile.ZipFile(file="integration_tests/resources.zip", mode='r') as zf:
         zf.extractall("integration_tests/resources/")
     with TempDir("integration_tests/resources"), TempFileLogger(os.path.join(log_dir, "integration_tests.log"), "DEBUG"):
--- testing/steps/unit_tests.py
+++ testing/steps/unit_tests.py
@@ -9,7 +9,7 @@ LOGGER = logging.getLogger(__name__)
 
 
 def unit_tests(*args, **kwargs):
-    log_dir = os.path.dirname(os.path.abspath(os.environ.get("LOG_FILE", "./logs/atf.log")))
+    log_dir = os.path.dirname(os.path.abspath(os.getenv("LOG_FILE", "./logs/atf.log")))
     with zipfile.ZipFile(file="unit_tests/resources.zip", mode='r') as zf:
         zf.extractall("unit_tests/resources/")
     with TempDir("unit_tests/resources"), TempFileLogger(os.path.join(log_dir, "unit_tests.log"), "DEBUG"):
